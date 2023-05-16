3ecadb52b564abced60d9ef134c1b6bf7ce5466a
某闪 <moushan@ywwl.com>
2023-05-16 11:47:27 +0800

    mod: spring降级 fix: 日期计算

--- MPDemo/src/main/java/com/planitary/message/push/Utils/DateUtils.java
+++ MPDemo/src/main/java/com/planitary/message/push/Utils/DateUtils.java
@@ -1,9 +1,12 @@
 package com.planitary.message.push.Utils;
 
 
+import org.springframework.stereotype.Component;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
@@ -82,11 +85,27 @@ public class DateUtils {
         Date birthDate;
         LocalDateTime birthDateTime = null;
         try {
+            // 拿到日期的月份，进行比对，如果生日已过，就计算明年的，生日未过，就计算今年的
             birthDate = simpleDateFormat.parse(birthDay);
             birthDateTime = dateConvert2LocalDateTime(birthDate);
+            int birthMonth = birthDateTime.getMonth().getValue();
+            int currentMonth = LocalDateTime.now().getMonth().getValue();
+            int currentYear = LocalDateTime.now().getYear();
+            if (currentMonth < birthMonth){
+                // 用当前的时间年份填充生日年份，计算相差值
+                LocalDateTime birthOfThisYear = birthDateTime.withYear(currentYear);
+                return getTimeDiff(LocalDateTime.now(),birthOfThisYear,"days");
+            }
+            else {
+                // 用明年的时间填充生日年份，计算差值
+                LocalDateTime birthOfNextYear = birthDateTime.withYear(currentYear + 1);
+                return getTimeDiff(LocalDateTime.now(),birthOfNextYear,"days");
+            }
+
+
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        return getTimeDiff(birthDateTime,LocalDateTime.now(),"days");
+        return 0L;
     }
 }
--- MPDemo/src/test/java/DateTest.java
+++ MPDemo/src/test/java/DateTest.java
@@ -1,12 +1,12 @@
 import com.planitary.message.push.Utils.DateUtils;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class DateTest {
 
-
     @Test
     public void BirthTest(){
-        String birthday = "1996-06-25";
+        String birthday = "1996-01-11";
         long currentDayToBirth = DateUtils.currentDayToBirth(birthday);
         System.out.println(currentDayToBirth);
     }
