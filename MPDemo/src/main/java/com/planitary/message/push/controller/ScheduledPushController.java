1b7dbd89ee34829b3669cb9688c8912822cc8c88
某闪 <moushan@ywwl.com>
2023-05-24 11:03:51 +0800

    mod：推送id

--- MPDemo/src/main/java/com/planitary/message/push/controller/ScheduledPushController.java
+++ MPDemo/src/main/java/com/planitary/message/push/controller/ScheduledPushController.java
@@ -7,6 +7,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Random;
 
 @Component
 @Slf4j
@@ -17,7 +18,10 @@ public class ScheduledPushController {
 
     @Scheduled(cron = "0 0/1 * * * ? ")
     public void Push() throws IOException {
-        log.info("开始推送");
+        Random random = new Random();
+        int prefix = random.nextInt(99);
+        String pushId = String.valueOf(prefix + System.currentTimeMillis());
+        log.info("开始推送,推送id:{}",pushId);
         pushService.push();
     }
 }
